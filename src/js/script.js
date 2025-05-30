1e1a603d5f4efaaffb5fe29f327eb05fd6615377
camila <camilamendonca777@gmial.com>
2025-05-30 19:43:11 -0300

    construindo o slideshow em HTML CSS e JAVASCRIPT

---
+++ src/js/script.js
@@ -0,0 +1,36 @@
+let slideIndex = 0; //variavel slideIndex com 0. ela controla o slide
+
+function mostrarSlides() { //informa que a função mostrarSlides será a responsável e inicia a declaração de tudo oq vai acontecer nela
+  const slides = document.getElementsByClassName("slide_image"); //pega todos os elementos dessa classe
+  const pontos = document.getElementsByClassName("ponto");  //pega todos os elementos dessa classe
+
+ 
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";   //esconde todos os slides
+  }
+
+ 
+  for (let i = 0; i < pontos.length; i++) {
+    pontos[i].classList.remove("ativo");    //remove o branco de todas as bolinhas
+  }
+
+  
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1; //aumenta a variavel slideIndex para 1 para aparecer a imagem
+  }
+
+  // Exibe o slide atual
+  slides[slideIndex - 1].style.display = "block";   //mostra o slide
+
+  // Destaca a bolinha correspondente
+  if (pontos.length >= slideIndex) {
+    pontos[slideIndex - 1].classList.add("ativo");  //adiciona a classe ativo á bolinha, assim mostrando ela
+  }
+
+ 
+  setTimeout(mostrarSlides, 3000);  //chama essa função a cada 3s
+}
+
+
+mostrarSlides();    //faz a função funcionar
